fffd8832beb8cb353af246fff27495b804cc3a59
secchipietro04 <secchi.pietro04@gmail.com>
2024-05-08 01:01:05 +0200

    python implementation of the solution

---
+++ TestCaseGenerator.py
@@ -0,0 +1,83 @@
+import argparse
+import random
+
+class Product:
+    ID = 0
+    duration = 0
+    expiration = 0
+    priority = 0
+
+    def __init__(self, ID, duration, expiration, priority):
+        self.ID = ID
+        self.duration = duration
+        self.expiration = expiration
+        self.priority = priority
+
+    def calculatePenal(self, time):
+        return self.priority*(time-self.expiration)
+
+    def __str__(self):
+        return f"Identificativo: {self.ID}; Durata: {self.duration}; Scadenza: {self.expiration}; Priorità: {self.priority};\n"
+MAX_ID = 127
+MAX_PRIORITY = 5
+MAX_EXPIRATION = 100
+MAX_DURATION = 10
+
+
+def positive_int(value):
+    ivalue = int(value)
+    if ivalue <= 0:
+        raise argparse.ArgumentTypeError(
+            "%s is not a positive integer" % value)
+    return ivalue
+
+
+
+
+def generate_test_cases(num_test_cases, max_priority=None, max_expiration=None, max_duration=None):
+    if (not max_priority):
+        max_priority = MAX_PRIORITY
+    if (not max_expiration):
+        max_expiration = MAX_EXPIRATION
+    if (not max_duration):
+        max_duration = MAX_DURATION
+    test_cases = {}
+    for id in range(num_test_cases):
+        duration = random.randint(1, max_duration)
+        expiration = random.randint(1, max_expiration)
+        priority = random.randint(1, max_priority)
+        test_cases[id] = Product(id, duration, expiration, priority)
+    return test_cases
+
+
+def write_test_cases_to_file(filename, test_cases):
+    with open(filename, 'w', encoding='utf-8') as file:
+        for i in test_cases:
+            test_case = test_cases[i]
+            file.write(str(test_case))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Generate test cases and write them to a file')
+    parser.add_argument('num_test_cases', type=positive_int,
+                        help='Number of test cases')
+    parser.add_argument('--max_priority', type=positive_int,
+                        help='Maximum number of priorities')
+    parser.add_argument('--max_ids', type=positive_int,
+                        help='Maximum number of priorities')
+    parser.add_argument('--max_expiration', type=positive_int,
+                        help='Maximum number of expirations')
+    parser.add_argument('--max_duration', type=positive_int,
+                        help='Maximum number of durations')
+    parser.add_argument('--filename', type=str,
+                        default='test_cases.txt', help='Name of the output file')
+    args = parser.parse_args()
+
+    # test wether there are more ids then test cases
+    assert ((MAX_ID if not args.max_ids else args.max_ids) > args.num_test_cases)
+
+    test_cases = generate_test_cases(
+        args.num_test_cases, args.max_priority, args.max_expiration, args.max_duration)
+    write_test_cases_to_file(args.filename, test_cases)
+    print(f"Test cases written to {args.filename}")
---
+++ pianificatore.py
@@ -0,0 +1,109 @@
+import argparse
+
+
+class Product:
+    ID = 0
+    duration = 0
+    expiration = 0
+    priority = 0
+
+    def __init__(self, ID, duration, expiration, priority):
+        self.ID = ID
+        self.duration = duration
+        self.expiration = expiration
+        self.priority = priority
+
+    def calculatePenalty(self, time):
+        if (time<=self.expiration): return 0
+        return self.priority*(time-self.expiration)
+
+    def __str__(self):
+        return f"Identificativo: {self.ID}; Durata: {self.duration}; Scadenza: {self.expiration}; Priorità: {self.priority};\n"
+
+    def from_string(string):
+        parts = string.split(';')
+        ID = int(parts[0].split(': ')[1].strip())
+        duration = int(parts[1].split(': ')[1].strip())
+        expiration = int(parts[2].split(': ')[1].strip())
+        priority = int(parts[3].split(': ')[1].strip())
+        product = Product(ID, duration, expiration, priority)
+        return product
+
+
+
+def load(filename):
+    products = []
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            products.append(Product.from_string(line))
+    return products
+
+
+def loadPriorities(method, products):
+    queues = {}
+    assert (method == "EDF" or method == "HPF")
+    if method == 'HPF':
+        for product in products:
+            if (product.priority in queues):
+                queues[product.priority].append(product)
+            else:
+                queues[product.priority] = [product]
+    else:
+        for product in products:
+            if (product.expiration in queues):
+                queues[product.expiration].append(product)
+            else:
+                queues[product.expiration] = [product]
+
+    return queues
+
+
+def sortQueues(method, products):
+    assert (method == "EDF" or method == "HPF")
+    if method == 'EDF':
+        for queue in products:
+
+            products[queue] = sorted(products[queue], key=lambda x: x.priority, reverse=True)
+        products = dict(sorted(products.items()))
+    else:
+        for queue in products:
+
+            products[queue] = sorted(products[queue], key=lambda x: x.expiration)
+        products = dict(sorted(products.items(), reverse=True))
+    return products
+
+def simulate(products):
+    penalty=0
+    simulated=[]
+    time=0
+    for idx in products:
+        queue=products[idx]
+        for item in queue:
+            simulated.append(f"{item.ID}:{time}")
+            time+=item.duration
+            penalty+=item.calculatePenalty(time)
+    return penalty, simulated, time
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description='Simulate the asm planner')
+    parser.add_argument('filename', type=str,
+                        help='name of the file with the products')
+
+    args = parser.parse_args()
+    alg = int(input("quale algoritmo si vuole usare?:"))
+    method = ""
+    assert (alg == 1 or alg == 2)
+    if alg == 1:
+        method = "EDF"
+    if alg == 2:
+        method = "HPF"
+    products = load(args.filename)
+    products = loadPriorities(method,products)
+    products = sortQueues(method,products)
+    penalty, simulated, time=simulate(products)
+    for i in simulated:
+        print(i)
+    print(f"Conclusione: {time}")
+    print(f"Penalty: {penalty}")
